60a1d08814979feda5dd7a7e3a6100f286ffb6f3
root <root@thor>
2017-05-07 21:48:25 +0000

    Removed thumbnail

--- content.php
+++ content.php
@@ -6,16 +6,9 @@
   </div>
   <div class="row">
 
-    <div class="col-md-8">
+    <div class="col-md-12">
       <p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
        <?php the_excerpt(); ?>
-    </div>
-    <div class="col-md-4">
-      <a href="<?php the_permalink(); ?>">
-        <?php if ( has_post_thumbnail() ) {
-          the_post_thumbnail();
-        } ?>
-      </a>
     </div>
   </div><!-- /.blog-post -->
 
