c46d4d58452493eab0557b1a75b9a8b78fef85a5
Soren Daugaard <sbd@ineptum.dk>
2017-05-16 20:03:06 -0400

    Updated styles

--- content.php
+++ content.php
@@ -9,6 +9,9 @@
     <div class="col-md-12">
       <p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
        <?php the_excerpt(); ?>
+       <div class="right">
+         <a href="<?php the_permalink(); ?>" class="right">Read More</a>         
+       </div>
     </div>
   </div><!-- /.blog-post -->
 
--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form class="form-vertical" role="search">
+<form class="form-vertical" role="search" action="/">
   <div class="form-group">
     <input type="text" name="s" class="form-control" placeholder="Search">
     </div>
